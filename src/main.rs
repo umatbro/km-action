0fbf182d9daf84f6bf9dc3aed3aa50c8866c237d
umatbro <umatbroo@gmail.com>
2023-02-10 00:31:26 +0100

    Start read PR Number

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,12 @@ use std::env;
 use std::fs::write;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let name = args.get(1).expect("Name not provided");
-    println!("Hello, {name}!");
     set_github_output_env();
 }
 
 fn set_github_output_env() {
     let current_time = Local::now().to_string();
-    let env_val = format!("time={current_time}");
-    // env::set_var("GITHUB_OUTPUT", env_val);
+    let env_val = format!("time={current_time},pr_number=[PR-NUM]");
     let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
     write(github_output_path, env_val).unwrap();
 }
