eef3eaaad4bd45fd3230410f77cab4da57a7d79e
umatbro <umatbroo@gmail.com>
2023-02-10 00:05:27 +0100

    Improve modifying output value

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
 use chrono::Local;
 use std::env;
+use std::fs::write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,5 +12,7 @@ fn main() {
 fn set_github_output_env() {
     let current_time = Local::now().to_string();
     let env_val = format!("time={current_time}");
-    env::set_var("GITHUB_OUTPUT", env_val);
+    // env::set_var("GITHUB_OUTPUT", env_val);
+    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
+    write(github_output_path, env_val).unwrap();
 }
