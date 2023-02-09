2cb513e5e99a3aef442e34e3c2601193b91e8ce8
umatbro <umatbroo@gmail.com>
2023-02-10 00:58:10 +0100

    Support multiple outputs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use chrono::Local;
 use std::env;
 use std::fs::write;
+use serde_json::json;
 
 fn main() {
     set_github_output_env();
@@ -8,7 +9,14 @@ fn main() {
 
 fn set_github_output_env() {
     let current_time = Local::now().to_string();
-    let env_val = format!("time={current_time},pr_number=[PR-NUM]");
-    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
-    write(github_output_path, env_val).unwrap();
+    // let env_val = format!("time={current_time},pr_number=[PR-NUM]");
+    let outputs = json!({
+        "time": current_time,
+        "pr_number": "[PR-NUM]",
+    });
+    let github_output_path = env::var("GITHUB_OUTPUT");
+    match github_output_path {
+        Ok(v) => write(v, outputs.to_string()).unwrap(),
+        Err(_e) => env::set_var("GITHUB_OUTPUT", outputs.to_string()),
+    }
 }
