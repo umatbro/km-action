fcaf8427f7f075fbc75a09dbf27be656f44b95de
umatbro <umatbroo@gmail.com>
2023-02-10 02:26:06 +0100

    Read data from env vars

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use std::env;
-use std::fs::write;
+use std::fs::{File, write};
+use std::io::Read;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let empty_msg = String::from("<EMPTY>");
-    let pull_req_obj = args.get(1).unwrap_or(&empty_msg);
-    println!("Pull req obj: {}", pull_req_obj);
     set_github_output_env();
     print_pr_link();
 }
@@ -19,9 +16,24 @@ fn set_github_output_env() {
     }
 }
 
+/// `GITHUB_EVENT_PATH`
+///
+/// The path to the file on the runner that contains the full event webhook payload.
+/// For example, `/github/workflow/event.json`.
+///
+/// https://docs.github.com/en/actions/learn-github-actions/variables
 fn print_pr_link() {
     println!(
         "Github token: {}",
         env::var("GITHUB_TOKEN").unwrap_or("<missing>".to_string())
     );
+    let event_path = env::var("GITHUB_EVENT_PATH");
+    if let Err(e) = event_path {
+        panic!("GITHUB_EVENT_PATH not found.");
+    }
+    let p = event_path.unwrap();
+    let mut f = File::open(p).unwrap();
+    let mut buf = String::new();
+    f.read_to_string(&mut buf).unwrap();
+    println!("Data from GITHUB_EVENT_PATH: {}", buf);
 }
