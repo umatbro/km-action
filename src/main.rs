bc2125565dd83a46f6ba1405afd0e600dff49466
umatbro <umatbroo@gmail.com>
2023-02-10 01:25:34 +0100

    Only output one val

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,10 @@ fn main() {
 
 fn set_github_output_env() {
     let current_time = Local::now().to_string();
-    // let env_val = format!("time={current_time},pr_number=[PR-NUM]");
-    let outputs = json!({
-        "time": current_time,
-        "pr_number": "[PR-NUM]",
-    });
+    let env_val = format!("time={current_time},pr_number=[PR-NUM]");
     let github_output_path = env::var("GITHUB_OUTPUT");
     match github_output_path {
-        Ok(v) => write(v, outputs.to_string()).unwrap(),
-        Err(_e) => env::set_var("GITHUB_OUTPUT", outputs.to_string()),
+        Ok(v) => write(v, env_val).unwrap(),
+        Err(_e) => env::set_var("GITHUB_OUTPUT", env_val),
     }
 }
